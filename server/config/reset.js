0dbf9291f755f918a1520a4d60fe0d25356c4b03
Htetty <htetswork@gmail.com>
2025-10-22 17:43:14 -0700

    Fix reset.js implementation

--- server/config/reset.js
+++ server/config/reset.js
@@ -3,3 +3,7 @@ import "./dotenv.js";
 import { fileURLToPath } from "url";
 import path, { dirname } from "path";
 import fs from "fs";
+
+const currentPath = fileURLToPath(import.meta.url);
+
+const tripsData = JSON.parse(tripsFile);
